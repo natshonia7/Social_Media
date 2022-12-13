bc0c0c00f998e4ce572f3f03dbeaf4aa2a72a821
natia <nati.shonia37@gmail.com>
2022-12-13 16:53:31 +0400

    Create Topbar.js

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./topbar.css";
+import Search from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Social_Media</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            type="text"
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+          <div className="topbarLinks">
+             <span className="topbarLink">Homepage</span>
+             <span className="topbarLink">Timeline</span>
+          </div>
+
+          <div className="topbarIcons">
+
+             <div className="topbarIconItem">
+                   <PersonIcon />
+                   <span className="topbarIconBadge">1</span>
+             </div>
+
+             <div className="topbarIconItem">
+                   <ChatIcon />
+                  <span className="topbarIconBadge">2</span>
+             </div>
+
+             <div className="topbarIconItem">
+                   <NotificationsIcon />
+                   <span className="topbarIconBadge">1</span>
+          </div>
+
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
